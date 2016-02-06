b36610fd797943dcebf596d3c5bcc37ae1918c1f
Kostas Georgiadis <kongeor@gmail.com>
2016-02-06 19:11:52 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to potoo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/potoo/core_test.clj
@@ -0,0 +1,7 @@
+(ns potoo.core-test
+  (:require [clojure.test :refer :all]
+            [potoo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
