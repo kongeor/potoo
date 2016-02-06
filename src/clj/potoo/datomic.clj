a5cac220e0188e308edec70e5f8e42859c4c7369
Kostas Georgiadis <kongeor@gmail.com>
2016-02-06 19:29:43 +0200

    Create datomic schema, initial data and component

---
+++ src/clj/potoo/datomic.clj
@@ -0,0 +1,40 @@
+(ns potoo.datomic
+  (:require [datomic.api :as d]
+            [com.stuartsierra.component :as component]
+            [clojure.java.io :as io])
+  (:import datomic.Util))
+
+
+;; Queries
+
+(defn find-potoos [conn]
+  (d/q '[:find ?text ?author ?created
+         :where [?p :pot/text ?text]
+                [?p :pot/author ?author]
+                [?p :pot/created ?created]]
+       (d/db conn)))
+
+;; Component
+
+(defrecord DatomicDatabase [uri schema initial-data db-conn]
+  component/Lifecycle
+  (start [component]
+    (d/create-database uri)
+    (let [c (d/connect uri)]
+      @(d/transact c schema)
+      @(d/transact c initial-data)
+      (assoc component :db-conn c)))
+  (stop [component]
+    component))
+
+(defn new-database [db-uri]
+  (DatomicDatabase.
+    db-uri
+    (first (Util/readAll (io/reader (io/resource "datomic/schema.edn"))))
+    (first (Util/readAll (io/reader (io/resource "datomic/initial.edn"))))
+    nil))
+
+(comment
+  (let [datomic (new-database "datomic:mem://localhost:4334/potoos")
+        conn (-> (.start datomic) :db-conn)]
+       (find-potoos conn)))
