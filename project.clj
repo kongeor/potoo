3a95d600cbebb5c834bcc6db49ac598c1a34f084
Kostas Georgiadis <kongeor@gmail.com>
2016-02-06 19:18:33 +0200

    Update project.clj and create clj source path

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject potoo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A mini twitter clone backend using clojure and datomic"
+  :url "https://github.com/kongeor/potoo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+
+  :source-paths ["src/clj"])
