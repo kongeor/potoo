be48d2ad32ff7e518d20a9c9a7f1febb93d5bff8
Kostas Georgiadis <kongeor@gmail.com>
2016-02-16 19:01:15 +0200

    Implement the basic core.cljs to list potoos

--- project.clj
+++ project.clj
@@ -4,13 +4,28 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [reagent "0.6.0-alpha"]
+                 [cljs-ajax "0.5.3"]
                  [com.datomic/datomic-free "0.9.5344"]
                  [com.stuartsierra/component "0.3.1"]
                  [ring/ring "1.4.0"]
                  [ring/ring-json "0.4.0"]
                  [bidi "1.25.0"]
                  [com.taoensso/timbre "4.2.1"]]
-
   :source-paths ["src/clj"]
   :main potoo.system
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.0.0"
+  :plugins [[lein-figwheel "0.5.0-1"]]
+  :clean-targets [:target-path "out"]
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src/cljs"]
+                        :figwheel true
+                        :compiler {:main "potoo.core"
+                                   :asset-path "js/out"
+                                   :output-to "resources/public/js/app.js"
+                                   :output-dir "resources/public/js/out"}
+                        }]
+              }
+  )
---
+++ src/cljs/potoo/core.cljs
@@ -0,0 +1,39 @@
+(ns potoo.core
+  (:require [reagent.core :as r]
+            [ajax.core :refer [GET POST]]))
+
+(def app-state
+  (r/atom
+    {:potoos
+     [{:key 1 :text "Ow man" :author "Morty"}
+      {:key 2 :text "Don't judge" :author "Rick"}]}))
+
+(defn potoo [p]
+  [:li
+   [:span (:text p)]
+   [:span ", "]
+   [:span (:name p)]
+   [:span ", "]
+   [:span (:date p)]])
+
+(defn potoo-list []
+  [:div
+   [:h1 "Potooooooos!"]
+   [:ul
+    (for [p (:potoos @app-state)]
+      [potoo p])]])
+
+(defn start []
+  (r/render-component
+    [potoo-list]
+    (.getElementById js/document "app")))
+
+(start)
+
+(defn handler [response]
+  (swap! app-state assoc :potoos response))
+
+(GET "/api/potoos" {:keywords? true
+                    :response-format :json
+                    :handler handler})
+
