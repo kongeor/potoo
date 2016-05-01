6bf75add70dd649419deb56ece32dcde193fc071
Kostas Georgiadis <kongeor@gmail.com>
2016-05-01 18:13:53 +0300

    Fix build and update deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/kongeor/potoo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.34"]
                  [reagent "0.6.0-alpha"]
                  [cljs-ajax "0.5.3"]
                  [com.datomic/datomic-free "0.9.5344"]
@@ -18,14 +18,15 @@
                  [bouncer "1.0.0"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-  :main potoo.system
   :min-lein-version "2.0.0"
   :plugins [[lein-figwheel "0.5.0-1"]
             [lein-cljsbuild "1.1.3"]]
-  :clean-targets [:target-path "out"]
+  :clean-targets ^{:protect false} ["resources/public/js"
+                                    :target-path]
   :uberjar-name "potoo-standalone.jar"
-  :profiles {:uberjar {:aot :all}}
-  :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
+  :profiles {:uberjar {:prep-tasks ["compile" ["cljsbuild" "once" "min"]]
+                       :aot :all}
+             :dev {:source-paths ["dev"]}}
   :cljsbuild {
               :builds [{:id "dev"
                         :source-paths ["src/cljs" "src/cljs_dev"]
