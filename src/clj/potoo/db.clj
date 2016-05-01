1468ce455175f3d6ed02cbebcf1a6ddb0aef2870
Kostas Georgiadis <kongeor@gmail.com>
2016-05-01 18:35:50 +0300

    Create postgres db connection

---
+++ src/clj/potoo/db.clj
@@ -0,0 +1,61 @@
+(ns potoo.db
+  (:require
+    [clojure.java.jdbc :as jdbc]
+    [jdbc.pool.c3p0 :as pool]
+    [migratus.core :as migratus]
+    [taoensso.timbre :as log]
+    [com.stuartsierra.component :as component]
+    [clojure.string :as str])
+  (:import (java.net URI)))
+
+;; =============
+;; DB connection
+
+(defn- get-user-and-password [uri]
+  (if (nil? (.getUserInfo uri))
+    nil (str/split (.getUserInfo uri) #":")))
+
+(defn- get-db-spec [uri]
+  {:classname        "org.postgresql.Driver"
+   :subprotocol      "postgresql"
+   :user             (get (get-user-and-password uri) 0)
+   :password         (get (get-user-and-password uri) 1)
+   :subname          (if (= -1 (.getPort uri))
+                       (format "//%s%s" (.getHost uri) (.getPath uri))
+                       (format "//%s:%s%s" (.getHost uri) (.getPort uri) (.getPath uri)))})
+
+(defn- make-db-spec [uri]
+  (pool/make-datasource-spec (log/spy (get-db-spec uri))))
+
+(defn- migrate-db [uri]
+  (let [config {:store :database
+                :db (get-db-spec uri)}]
+    (log/info "Applying migrations")
+    (migratus/migrate config)))
+
+(defn- get-db-uri [uri]
+  (URI.
+    (or
+      (System/getenv "DATABASE_URL")
+      uri)))
+
+(defrecord PostgresDatabase [uri]
+  component/Lifecycle
+  (start [component]
+    (log/info "Connecting to Postgres.")
+    (let [uri (get-db-uri uri)
+          db (make-db-spec uri)]
+      #_(migrate-db uri)
+      (assoc component :db-spec db)))
+  (stop [component]
+    (log/info "Closing the connection to Postgres")
+    (.close (:db-spec component))
+    component))
+
+(defn new-database [uri]
+  (PostgresDatabase. uri))
+
+(comment
+  (let [db (new-database "postgres://potoo:potoo@localhost:5432/potoo")
+        db (component/start db)]
+    (println db)))
